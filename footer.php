1be14804f2582b2a76f833740dc0c7c050b3f77a
키위 <kdo9921@apteryx.moe>
2021-01-09 17:17:05 +0900

    파비콘추가, footer 수정, css 수정

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 			<a href="<?php bloginfo( 'url' ); ?>"><?php bloginfo( 'name' ); ?></a> All rights reserved.
 		</div>
 		<div class="theme-info">
-			KiwiSkin Theme by <a href="https://apteryx.moe">Kiwi</a>
+			<a href="https://github.com/kdo9921/KiwiSkin-for-Wordpress">KiwiSkin</a> Theme by <a href="https://apteryx.moe">Kiwi</a>
 		</div>
 	</div>
 	<?php wp_footer(); ?>
