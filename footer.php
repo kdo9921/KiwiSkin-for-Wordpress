e5c7ef3bc38c4c2c64d7d23b8408a4cbb8b27213
키위 <kdo9921@apteryx.moe>
2021-01-10 05:26:28 +0900

    Fxxx IE

--- footer.php
+++ footer.php
@@ -5,7 +5,8 @@
 			<a href="<?php bloginfo( 'url' ); ?>"><?php bloginfo( 'name' ); ?></a> All rights reserved.
 		</div>
 		<div class="theme-info">
-			<a href="https://github.com/kdo9921/KiwiSkin-for-Wordpress">KiwiSkin</a> Theme by <a href="https://apteryx.moe">Kiwi</a>
+			<a href="https://github.com/kdo9921/KiwiSkin-for-Wordpress">KiwiSkin</a> Theme by <a
+				href="https://apteryx.moe">Kiwi</a>
 		</div>
 	</div>
 	<?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -5,12 +5,15 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name='viewport' content='initial-scale=1, maximum-scale=1.0, user-scalable=no" viewport-fit=cover'>
     <title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
-
+    <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
     <?php wp_head() ; ?>
 </head>
 
 <body <?php body_class(); ?>>
+
     <header class="site-header">
-        <a href="/"><h1><?php bloginfo('name') ?><h1></a>
+        <a href="/">
+            <h1><?php bloginfo('name') ?><h1>
+        </a>
     </header>
