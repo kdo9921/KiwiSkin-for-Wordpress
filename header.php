78fecfcabad9e8f52ab86f4bbeb0e735f30ace0a
키위 <kdo9921@apteryx.moe>
2021-01-17 21:39:24 +0900

    타이틀 수정 출력

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name='viewport' content='initial-scale=1, maximum-scale=1.0, user-scalable=no" viewport-fit=cover'>
-    <title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
+    <title><?php wp_title('-', true, 'right'); ?></title>
     <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
     <?php wp_head() ; ?>
