397fd94c5094224a1bcc5ca117d9512837666ebb
키위 <kiwi@apteryx.moe>
2021-04-16 02:44:15 +0900

    색상 커스텀 기능

--- functions.php
+++ functions.php
@@ -44,4 +44,63 @@ function triki_register_menus() {
 add_action('init', 'triki_register_menus');
 
 
+
+function mytheme_customize_register( $wp_customize ) {
+    $wp_customize->add_setting( 'main_color' , array(
+        'default'     => "#9ce159",
+        'transport'   => 'refresh',
+    ) );
+    $wp_customize->add_setting( 'sub_color' , array(
+        'default'     => "#8c5e29",
+        'transport'   => 'refresh',
+    ) );
+    $wp_customize->add_setting( 'header_color_1' , array(
+        'default'     => "#9ce159",
+        'transport'   => 'refresh',
+    ) );
+    $wp_customize->add_setting( 'header_color_2' , array(
+        'default'     => "#8c5e29",
+        'transport'   => 'refresh',
+    ) );
+    $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'main_color', array(
+        'label'        => __( 'Main Color', 'mytheme' ),
+        'section'    => 'colors',
+    ) ) );
+    $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'sub_color', array(
+        'label'        => __( 'Sub Color', 'mytheme' ),
+        'section'    => 'colors',
+    ) ) );
+    $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'header_color_1', array(
+        'label'        => __( 'Header Color 1', 'mytheme' ),
+        'section'    => 'colors',
+    ) ) );
+    $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'header_color_2', array(
+        'label'        => __( 'Header Color 2', 'mytheme' ),
+        'section'    => 'colors',
+    ) ) );
+}
+add_action( 'customize_register', 'mytheme_customize_register' );
+
+function mytheme_customize_css()
+{
+    ?>
+    <style type="text/css">
+        header { 
+            background: linear-gradient(120deg, <?php echo get_theme_mod('header_color_1', "#9ce159"); 
+        ?>, <?php echo get_theme_mod('header_color_2', "#8c5e29"); ?>);
+        }
+        article a,article a:visited,.site-footer a,.site-footer a:visited {
+            color: <?php echo get_theme_mod('main_color', "#9ce159"); ?>;
+        }
+        .widget_title { 
+            border-bottom: 2px solid <?php echo get_theme_mod('main_color', "#9ce159"); ?>;
+        }
+        article a:hover, .site-footer a:hover {
+            color: <?php echo get_theme_mod('header_color_1', "#8c5e29"); ?>;
+        }
+    </style>
+    <?php
+}
+add_action( 'wp_head', 'mytheme_customize_css');
+
 ?>
